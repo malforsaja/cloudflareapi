e16c65afe5e6f62e30e682a0571b6580c9fdc9e5
malforsaja <malfor.saja@gmail.com>
2018-03-19 23:59:13 +0100

    get ipv4 and ipv6

--- index.js
+++ index.js
@@ -8,14 +8,19 @@ var cf = require('cloudflare')({
     key: 'b66001ad605220c35a347572d568da9e9c2d4'
 });
 
-cf.ips.browse()
+cf.ips.browse().then(function(res){
+    console.log(res)
+})
 
 /*cf.zones.read('b66001ad605220c35a347572d568da9e9c2d4').then(function (resp) {
     return resp.result.status;
 });*/
 
 app.get('/test', (req, res) => {
-    console.log(cf.ips.browse())
+    //console.log(cf.ips.browse())
+    cf.ips.browse().then(function (response) {
+        res.status(200).json(response)
+    })
 })
 
 const port = 3000
